ba6f86813760612625a4092584ddc8f7c90fcd7f
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-11 13:35:58 -0300

    Initialized Spring Boot project with ScreenmatchApplication as the main entry point
    Set up the main class ScreenmatchApplication to serve as the entry point for the Spring Boot application. This class implements CommandLineRunner to execute code at startup.

---
+++ src/main/java/br/org/FelipeBert/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.org.FelipeBert.screenmatch;
+
+import br.org.FelipeBert.screenmatch.model.DadosSerie;
+import br.org.FelipeBert.screenmatch.service.ConsumoApi;
+import br.org.FelipeBert.screenmatch.service.ConverterDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?i=tt3896198&apikey=key");
+
+		System.out.println(json);
+
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}

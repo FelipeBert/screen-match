d25756d58bf54389471b36d4c13542f4b7eec4cc
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-11 13:37:57 -0300

    Implemented ConverterDados service to parse and convert JSON responses to Java objects using ObjectMapper.
    Implemented the ConverterDados service, which uses Jackson's ObjectMapper to deserialize JSON responses into Java objects. The service is designed to work with any class type, as defined by the IConverteDados interface.

---
+++ src/main/java/br/org/FelipeBert/screenmatch/service/ConverterDados.java
@@ -0,0 +1,14 @@
+package br.org.FelipeBert.screenmatch.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverterDados implements IConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) throws JsonProcessingException {
+        return mapper.readValue(json, classe);
+    }
+}

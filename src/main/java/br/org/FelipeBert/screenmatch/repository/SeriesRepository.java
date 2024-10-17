988bcd3bd28d26175a534161c337abde09e10b7f
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-17 19:42:45 -0300

    Add repository methods for filtering series and episodes by title, actor, category, seasons, and ratings
    - Implemented new methods to query series and episodes by various criteria, such as title, actor name, category, and number of seasons. Additionally, added custom queries to retrieve the top episodes based on ratings and series released after a certain year.

--- src/main/java/br/org/FelipeBert/screenmatch/repository/SeriesRepository.java
+++ src/main/java/br/org/FelipeBert/screenmatch/repository/SeriesRepository.java
@@ -1,7 +1,32 @@
 package br.org.FelipeBert.screenmatch.repository;
 
+import br.org.FelipeBert.screenmatch.model.Categoria;
+import br.org.FelipeBert.screenmatch.model.Episodio;
 import br.org.FelipeBert.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SeriesRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String titulo);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    @Query(value = "SELECT s from Serie s WHERE s.avaliacao >= :avaliacao AND s.totalTemporadas <= :numeroTemporadas")
+    List<Serie> seriesPorTemporadasEAvaliacao(Integer numeroTemporadas, Double avaliacao);
+
+    @Query(value = "SELECT e from Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
+    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
+
+    @Query(value = "SELECT e from Serie s JOIN s.episodios e WHERE s =:serie ORDER BY e.avaliacao DESC LIMIT 5")
+    List<Episodio> topEpisodiosPorSerie(Serie serie);
+
+    @Query(value = "SELECT e from Serie s JOIN s.episodios e WHERE s = :serieBuscada AND YEAR(e.lancamento) >= :anoLancamento")
+    List<Episodio> episodiosPorSerieEAno(Serie serieBuscada, Integer anoLancamento);
 }

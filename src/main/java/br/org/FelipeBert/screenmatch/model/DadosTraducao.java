28a2d0995c083cf9c4e64ee3b996a6d302b2c28b
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-17 09:10:05 -0300

    Add Jackson annotations for translation model
    Added Jackson annotations for the translation response models DadosTraducao and TextoTraduzido to map API responses properly.

---
+++ src/main/java/br/org/FelipeBert/screenmatch/model/DadosTraducao.java
@@ -0,0 +1,8 @@
+package br.org.FelipeBert.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTraducao(@JsonAlias("translatedText") String textoTraduzido) {
+}

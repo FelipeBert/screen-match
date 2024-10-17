cab39472032c526aef3fab0fbf6d1a3e04433922
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-17 09:08:46 -0300

    Create Episodio entity
    Added Episodio entity to represent series episodes with fields such as season number, episode number, title, and release date. Linked episodes to the series entity using a @ManyToOne relationship.

--- src/main/java/br/org/FelipeBert/screenmatch/model/Episodio.java
+++ src/main/java/br/org/FelipeBert/screenmatch/model/Episodio.java
@@ -1,14 +1,31 @@
 package br.org.FelipeBert.screenmatch.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+@Entity
+@Table(name = "episodios")
 public class Episodio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Integer numeroTemporada;
+
     private Integer numeroEpisodio;
+
     private String titulo;
+
     private Double avaliacao;
+
     private LocalDate lancamento;
+
+    @ManyToOne
+    private Serie serie;
+
+    public Episodio() {}
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.numeroTemporada = numeroTemporada;
@@ -24,6 +41,14 @@ public class Episodio {
         }catch (DateTimeParseException e){
             this.lancamento = null;
         }
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
 
     public Integer getNumeroTemporada() {

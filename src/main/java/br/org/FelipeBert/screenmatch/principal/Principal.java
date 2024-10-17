a9cca6593f5bceca472212a5db41199570443472
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-17 09:12:21 -0300

    Refactor Principal class for improved series search and translation handling
    - Added OMDB API key management using environment variables (System.getenv("OMDB_KEY")).
    - Refined the series search logic to handle spaces in series titles by encoding spaces as + in the URL.
    - Added translation functionality to fetch series synopsis in Portuguese using the MyMemory translation API.
    - Implemented sorting functionality to list series by genre.
    - Refactored code to improve error handling when fetching and storing series data, including cases where series are not found.

--- src/main/java/br/org/FelipeBert/screenmatch/principal/Principal.java
+++ src/main/java/br/org/FelipeBert/screenmatch/principal/Principal.java
@@ -1,112 +1,144 @@
 package br.org.FelipeBert.screenmatch.principal;
 
-import br.org.FelipeBert.screenmatch.model.DadosEpisodio;
-import br.org.FelipeBert.screenmatch.model.DadosSerie;
-import br.org.FelipeBert.screenmatch.model.DadosTemporada;
-import br.org.FelipeBert.screenmatch.model.Episodio;
+import br.org.FelipeBert.screenmatch.model.*;
+import br.org.FelipeBert.screenmatch.repository.SeriesRepository;
 import br.org.FelipeBert.screenmatch.service.ConsumoApi;
 import br.org.FelipeBert.screenmatch.service.ConverterDados;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class Principal {
     private final Scanner scanner = new Scanner(System.in);
 
     private final String ENDERECO = "http://www.omdbapi.com/?t=";
 
-    private final String API_KEY = "&apikey=key";
+    private final String ENDERECO_TRADUCAO = "https://api.mymemory.translated.net/get?q=";
+
+    private final String TRADUCAO_PTBR = "&langpair=";
+
+    private final String API_KEY = System.getenv("OMDB_KEY");
 
     private final ConsumoApi consumoApi = new ConsumoApi();
 
     private final ConverterDados conversor = new ConverterDados();
 
-    public void exibiMenu() throws JsonProcessingException {
-        System.out.println("Digite o Nome da Serie que deseja buscar: ");
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+    private List<Serie> series = new ArrayList<>();
+
+    private SeriesRepository repository;
+
+    public Principal(SeriesRepository repository) {
+        this.repository = repository;
+    }
+
+    public void exibiMenu() throws JsonProcessingException, UnsupportedEncodingException {
+        String menu = """
+                1 - Buscar Series
+                2 - Buscar Episodios
+                3 - Listar Series Buscadas
+                0 - Sair
+                """;
+        int opcao;
+        do {
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcao){
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção Invalida");
+            }
+        }while (opcao != 0 );
+        scanner.close();
+    }
+
+    private void listarSeriesBuscadas() {
+        series = repository.findAll();
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+    }
+
+    private void buscarSerieWeb() throws JsonProcessingException, UnsupportedEncodingException {
+        DadosSerie dadosBuscados = getDadosSerie();
+        Serie serie = new Serie(dadosBuscados);
+        //dadosSeries.add(dadosBuscados);
+        repository.save(serie);
+        System.out.println(dadosBuscados);
+    }
+
+    private DadosSerie getDadosSerie() throws JsonProcessingException, UnsupportedEncodingException {
+        System.out.println("Digite nome da Serie para busca: ");
         String nomeSerie = scanner.nextLine();
-        nomeSerie = nomeSerie.replace(" ", "+");
 
-        var json = consumoApi.obterDados(ENDERECO + nomeSerie + API_KEY);
-
+        String json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
 
-        System.out.println(dadosSerie + "\n");
+        String sinopseCodificada = URLEncoder.encode(dadosSerie.sinopse(), "UTF-8");
+        String traducaoEncode = URLEncoder.encode("en|pt-br");
 
-        List<DadosTemporada> temporadas = new ArrayList<>();
+        json = consumoApi.obterDados(ENDERECO_TRADUCAO + sinopseCodificada + TRADUCAO_PTBR + traducaoEncode).trim();
+        TextoTraduzido traducao = conversor.obterDados(json, TextoTraduzido.class);
 
-        for(int i = 1; i <= dadosSerie.totalTemporadas(); i++){
-            json = consumoApi.obterDados(ENDERECO + nomeSerie + "&season=" + i + API_KEY);
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
+        return new DadosSerie(
+                dadosSerie.titulo(),
+                dadosSerie.totalTemporadas(),
+                dadosSerie.atores(),
+                traducao.traducao().textoTraduzido(),
+                dadosSerie.genero(),
+                dadosSerie.poster(),
+                dadosSerie.avalaiacao()
+        );
+    }
+
+    private void buscarEpisodioPorSerie() throws JsonProcessingException, UnsupportedEncodingException {
+        System.out.println("Escolha uma Serie pelo Nome: ");
+        listarSeriesBuscadas();
+        String nome = scanner.nextLine();
+
+        Optional<Serie> serie = series.stream()
+                .filter(s -> s.getTitulo().toLowerCase().contains(nome.toLowerCase()))
+                .findFirst();
+
+        if(serie.isEmpty()){
+            System.out.println("Serie nao Encontrada");
         }
+        else{
+            Serie serieEncontrada = serie.get();
 
-        List<DadosEpisodio> dadosEpisodios = temporadas
-                .stream()
-                .flatMap(t -> t.episodios().stream())
-                .toList();
+            List<DadosTemporada> temporadas = new ArrayList<>();
+            for(int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++){
+                String json = consumoApi.obterDados(ENDERECO + serieEncontrada.getTitulo().replace(" ", "+") +"&season=" + i + API_KEY);
+                DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+                temporadas.add(dadosTemporada);
+            }
+            temporadas.forEach(System.out::println);
 
-        //.collect(Collectors.toList()) -> Permite adicao de novos dados;
-        //dadosEpisodios.add(new DadosEpisodio("teste", 3, "8","2020-01-01"));
+            List<Episodio> episodios = temporadas.stream()
+                    .flatMap(d -> d.episodios()
+                            .stream()
+                            .map(e -> new Episodio(d.numeroTemporada(), e)))
+                            .collect(Collectors.toList());
 
-//        dadosEpisodios.stream()
-//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-//                .peek(e -> System.out.println("Primeiro filtro N/A " + e))
-//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-//                .peek(e -> System.out.println("Ordenacao " + e))
-//                .limit(10)
-//                .peek(e -> System.out.println("Limite 10 " + e))
-//                .map(e -> e.titulo().toUpperCase())
-//                .peek(e -> System.out.println("Mapeamento " + e))
-//                .forEach(System.out::println);
-
-        List<Episodio> episodios = temporadas.stream()
-                .flatMap(t -> t.episodios().stream()
-                        .map(d -> new Episodio(t.numeroTemporada(), d) ) ).collect(Collectors.toList());
-
-//        System.out.println("Digite um Trecho do Titulo do episodio que deseja buscar: ");
-//        String trechoTitulo = scanner.nextLine();
-//
-//        Optional<Episodio> episodioBuscado = episodios.stream()
-//                                                    .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
-//                                                    .findFirst();
-//
-//        if(episodioBuscado.isEmpty()){
-//            System.out.println("Não foi possivel encontrar o episodio!");
-//        }
-//        else {
-//            System.out.println("Episodio encontrado, Pertence a " + episodioBuscado.get().getNumeroTemporada() + " Temporada");
-//        }
-
-        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
-                .filter(e -> e.getAvaliacao() > 0.0)
-                .collect(Collectors.groupingBy(Episodio::getNumeroTemporada,
-                        Collectors.averagingDouble(Episodio::getAvaliacao)));
-
-        System.out.println("Media das Temporadas");
-        System.out.println(avaliacoesPorTemporada);
-
-        DoubleSummaryStatistics est = episodios.stream()
-                .filter(e -> e.getAvaliacao() > 0.0)
-                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
-        System.out.println("Media " + est.getAverage());
-        System.out.println("Melhor Episodio " + est.getMax());
-        System.out.println("Pior Episodio " + est.getMin());
-        System.out.println("Quantidade Episodios avaliados " + est.getCount());
-
-        // episodios.forEach(System.out::println);
-        
-//        System.out.println("A partir de que ano voce deseja ver os episodios: ");
-//        int ano = scanner.nextInt();
-//        scanner.nextLine();
-//
-//        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
-//
-//        episodios.stream()
-//                .filter(e -> e.getLancamento() != null && e.getLancamento().isAfter(dataBusca))
-//                .forEach(System.out::println);
-
-        scanner.close();
+            serieEncontrada.setEpisodios(episodios);
+            repository.save(serieEncontrada);
+        }
     }
 }

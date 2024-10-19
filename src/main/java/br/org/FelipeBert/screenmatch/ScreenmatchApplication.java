c8c577c943595f8872e78b25395075cd09f6fdb2
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-10-19 15:21:31 -0300

    Update ScreenmatchApplication: standard Spring Boot web application setup
    Updated ScreenmatchApplication to a standard Spring Boot web application, removing the CommandLineRunner implementation and preparing the application to handle web requests with SpringApplication.run().

--- src/main/java/br/org/FelipeBert/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/org/FelipeBert/screenmatch/ScreenmatchApplication.java
@@ -1,27 +1,11 @@
 package br.org.FelipeBert.screenmatch;
 
-import br.org.FelipeBert.screenmatch.principal.Principal;
-import br.org.FelipeBert.screenmatch.repository.SeriesRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-    private SeriesRepository seriesRepository;
-
-    public ScreenmatchApplication(SeriesRepository seriesRepository) {
-        this.seriesRepository = seriesRepository;
-    }
-
+public class ScreenmatchApplication  {
     public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(seriesRepository);
-		principal.exibiMenu();
-	}
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 }
